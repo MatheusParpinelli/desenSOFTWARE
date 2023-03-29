79d0afa3a964b9ae9635523419067f04d8f1b919
Matheus Parpinelli <111715476+MatheusParpinelli@users.noreply.github.com>
2023-03-29 19:18:10 -0300

    aula2

---
+++ aula2/src/aula2/Principal.java
@@ -0,0 +1,49 @@
+package aula2;
+
+public class Principal {
+	// função == método
+	public static void main(String args[]) {
+		String texto1 = "Olá ";
+		String texto2 = " Matheus, ";
+		String texto3 = " sua média é: ";
+		String textoMensagem = texto1 + texto2 + texto3;
+		int valor = 2;
+		int valor2 = 10;
+		
+		System.out.println(textoMensagem + (valor + valor2)/2);
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
